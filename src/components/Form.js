22021e389cce7584d4de5bab02cfd48c7563bff8
sonnguyen2201 <n.vanson.2201@gmail.com>
2020-02-12 23:24:33 +0700

    fix

---
+++ src/components/Form.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+
+import axios from 'axios';
+
+class CustomForm extends React.Component {
+
+    handleFormSubmit = (event, requestType, articleID) => {
+        // event.preventDefault()
+        const title = event.target.elements.title.value;
+        const content = event.target.elements.content.value;
+
+        switch ( requestType ) {
+            case 'post':
+                return axios.post('http://127.0.0.1:8000/api/', {
+                    title: title,
+                    content: content
+                })
+                .then(res => console.log(res))
+                .catch(error => console.log(error));
+            case 'put':
+                return axios.put(`http://127.0.0.1:8000/api/${articleID}/`, {
+                    title: title,
+                    content: content
+                })
+                .then(res => console.log(res))
+                .catch(error => console.log(error));
+        }
+    }
+
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={(event) => this.handleFormSubmit(
+            event,
+            this.props.requestType,
+            this.props.articleID )}>
+          <Form.Item label="Title">
+            <Input name="title" placeholder="Put a title here..." />
+          </Form.Item>
+          <Form.Item label="Content">
+            <Input name="content" placeholder="Enter some content..." />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">{this.props.btnText}</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default CustomForm;
